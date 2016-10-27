fbe82d9c5037053674e9fd68ee6b62c5f1ef3374
brianLeeson <brianeleeson@gmail.com>
2016-10-27 16:47:55 -0700

    POI update

--- process_POI.py
+++ process_POI.py
@@ -7,7 +7,7 @@ def process():
 	with open('POI_trailheads.txt', 'r') as f:
 		read_data = f.readlines()
 	
-	for line ine read_data:
+	for line in read_data:
 		point = (line[0], line[1], line[2])
 		poi_data.append(point)
 	
